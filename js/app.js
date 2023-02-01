380f2d59ac0c3504b75f9280447217fd20707786
Rhuan Vitor <rhuanvitorpratti@gmail.com>
2023-02-01 11:18:03 -0300

    atualizandodenovo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 var url = window.location.href;
 var swLocation = '/Twittor.github.io/sw.js';
 if(navigator.serviceWorker){
-    if(url.includes(localhost)){
+    if(url.includes('localhost')){
         swLocation = '/sw.js'
     }
     navigator.serviceWorker.register(swLocation)
